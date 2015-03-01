adc111ae1ed514c6cb935a338f00688ff7513689
Balancer <balancer@balancer.ru>
2015-03-01 04:45:36 +0300

    init

---
+++ src/B2/Composer/Plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace B2\Composer;
+
+class Plugin implements Composer\Plugin\PluginInterface
+{
+	public function supports($packageType)
+	{
+		echo "test '$packageType'\n";
+		return 'bors-component' === $packageType;
+	}
+
+    public static function postAutoloadDump(Event $event)
+    {
+        $composer = $event->getComposer();
+        $io = $event->getIO();
+        $io->write('<info>Test: postAutoloadDump</info>');
+	}
+
+public function activate(Composer $composer, IOInterface $io)
+    {
+    	echo "activate\n";
+//        $installer = new TemplateInstaller($io, $composer);
+//        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
